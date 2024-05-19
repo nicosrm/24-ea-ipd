56917b4218fd2450069df1cdeabeacce6ec615c5
nicosrm <86198952+nicosrm@users.noreply.github.com>
2024-05-19 18:20:18 +0200

    Add IterativePrisonersDilemma struct.

---
+++ src/Sources/Structures/IterativePrisonersDilemma.swift
@@ -0,0 +1,23 @@
+//
+//  IterativePrisonersDilemma.swift
+//
+//  Created by nicosrm
+//
+
+import Foundation
+
+struct IterativePrisonersDilemma {
+    
+    let populationSize: Int
+    let memoryDepth: Int
+    let movesPerGame: Int
+    
+    let crossoverRate: Double
+    let mutationRate: Double
+    
+    let crossover: any CrossoverProtocol
+    let mutator: any MutationProtocol
+    let fitnessCalculator: any FitnessProtocol
+    
+//    let selectFunction: (Genotype, Genotype) -> Genotype
+}
