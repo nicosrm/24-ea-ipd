344dfeef4bb4634684fa3cd9c1e1e374a4d14d1b
nicosrm <86198952+nicosrm@users.noreply.github.com>
2024-05-19 18:16:05 +0200

    Add StrategyProtocol.

---
+++ src/Sources/Protocols/StrategyProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  StrategyProtocol.swift
+//
+//  Created by nicosrm
+//
+
+import Foundation
+
+protocol StrategyProtocol {
+    
+    typealias MoveHistory = [Move]
+    
+    /// Name of the strategy, e.g. "Random"
+    var name: String { get }
+    
+    /// Own history of last moves
+    var history: MoveHistory { get set }
+    
+    /// Make move based on passed opponent's history of moves.
+    /// Append made move to ``moveTable``.
+    /// 
+    /// - Returns: Move that was made
+    func makeMove(opponentHistory: MoveHistory) -> Move
+}
