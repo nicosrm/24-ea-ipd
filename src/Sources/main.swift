f326ee07a4953c288481be9f316f42ed1d00fb27
nicosrm <86198952+nicosrm@users.noreply.github.com>
2024-05-24 10:09:52 +0200

    [ADD] Test for crossover in main.

--- src/Sources/main.swift
+++ src/Sources/main.swift
@@ -1,4 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+let strategyA = GeneticStrategy(history: [.C, .C, .C])
+let strategyB = GeneticStrategy(history: [.C, .C, .C])
+
+print("strategy A:")
+strategyA.debugPrint()
+print()
+print("strategy B:")
+strategyB.debugPrint()
+print()
+
+let (childA, childB) = OnePointCrossover.crossover(strategyA, strategyB)
+
+print("child A")
+childA.debugPrint()
+print()
+print("child B")
+childB.debugPrint()
