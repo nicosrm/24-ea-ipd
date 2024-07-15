6bb40124de5769f70d6ae33230c57fd193f20004
nicosrm <86198952+nicosrm@users.noreply.github.com>
2024-07-15 16:34:29 +0200

    Add documentation to `StrategyScorePair`.

--- src/Sources/Structures/StrategyScorePair.swift
+++ src/Sources/Structures/StrategyScorePair.swift
@@ -6,6 +6,8 @@
 
 import Foundation
 
+/// Helper struct for ``GeneticStrategy`` with a score achieved during a match,
+/// tournament, selection procedure etc.
 struct StrategyScorePair {
     
     let strategy: GeneticStrategy
@@ -19,12 +21,13 @@ struct StrategyScorePair {
 
 extension [StrategyScorePair] {
     
-    var sortedByScore: Self {
+    var sortedByScore: [StrategyScorePair] {
         let sorted = self.sorted { $0.score > $1.score }
         return sorted
     }
 
-    var best: Self.Element? {
+    /// Best strategy, i.e. the strategy of the array with the highest score.
+    var best: StrategyScorePair? {
         return self.sortedByScore.first
     }
 }
